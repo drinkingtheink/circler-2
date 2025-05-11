8ccc3170c1cd1add2c0354f27596100eefb9fc40
drinkingtheink <jason.m.harrison@gmail.com>
2025-05-11 15:17:10 -0500

    meta

--- src/App.js
+++ src/App.js
@@ -292,7 +292,7 @@ const Circlescape = () => {
     <div className="circlescape-container">
       <div className={`sidebar-menu ${menuCollapsed ? 'collapsed' : ''}`}>
         <div className="menu-header">
-          <h2 className="app-title">Circlescape</h2>
+          <h2 className="app-title">The Circler</h2>
           <button 
             className="collapse-toggle" 
             onClick={() => setMenuCollapsed(!menuCollapsed)}
